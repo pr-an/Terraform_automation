7dba9191f0fb6c5a79c0a1680f2cba91fcd69c92
pr-an <107716211+pr-an@users.noreply.github.com>
2026-04-18 15:17:28 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = ""pranil-bucket-terraform-1""
+    bucket = "pranil-bucket-terraform-1"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
